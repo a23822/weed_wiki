3afa657c2f7ecf61a4a4465eed5754d2bfd9474d
a23822 <a238220@gmail.com>
2021-02-24 22:03:05 +0900

    카드 상세정보 보기 1차 완성 & props 정리

--- src/components/ncard/cardDetail.js
+++ src/components/ncard/cardDetail.js
@@ -19,7 +19,7 @@ const cardDetail = (props) => {
 
     // 닫기 버튼
     const onClickCloseBtn = () => {
-        getIsShowDetail(false);
+        getIsShowDetail(!isShowDetail);
     }
     
     // 라벨 인포메이션 버튼
@@ -27,6 +27,56 @@ const cardDetail = (props) => {
     const setIsPressedLabelInfoBtn = props.detailprops.setIsPressedLabelInfoBtn;
     const onClickLabelInfoBtn = (e) => {
         setIsPressedLabelInfoBtn(!isPressedLabelInfoBtn);
+    }
+
+    // 옵션목록 라벨로 변경
+    const classifyOptionToLabel = (option) => {
+        switch (option) {
+            case "모공":
+                return 'type_allAtk'
+            case "물공":
+                return 'type_pscAtk'
+            case "에공":
+                return 'type_engAtk'
+            case "생명":
+                return 'type_life'
+            case '공속':
+                return 'type_atkSpd'
+            case '방무':
+                return 'type_dpdPen'
+            case '쿨감':
+                return 'type_cooldn'
+            case '이속':
+                return 'type_movSpd'
+            case '치율':
+                return 'type_crtPer'
+            case '치피':
+                return 'type_crtDmgPer'
+            case '모방':
+                return 'type_allDpd'
+            case '물방':
+                return 'type_pscDpd'
+            case '에방':
+                return 'type_engDpd'
+            case '회복':
+                return 'type_regen'
+            case '상감':
+                return 'type_ccRdc'
+            case '독저':
+                return 'type_res'
+            case '냉저':
+                return 'type_res'
+            case '화저':
+                return 'type_res'
+            case '전저':
+                return 'type_res'
+            case '정저':
+                return 'type_res'
+            case '회피':
+                return 'type_dodge'
+            default:
+                return 'type_buff'
+        }
     }
 
     return (
@@ -47,7 +97,22 @@ const cardDetail = (props) => {
                                     <div className={styles.tag_group}>
                                         <button onClick={onClickLabelInfoBtn} className={`${styles.btn_label} type_${info_detail.rank.toLowerCase()}`} aria-pressed={isPressedLabelInfoBtn}>{info_detail.rank}<RiInformationLine className={styles.ico_info} aria-label="관련 정보 보기"/></button>
                                         <div className={styles.ly_sub}>
-                                            예시입니다
+                                            <strong className={styles.message}><RiInformationLine className={styles.ico_info}/>해당 태그들은 구할 수 있는 조건에 따라 임의로 나누었습니다.</strong>
+                                            <div className={styles.sub_info}>
+                                                <em className={`${styles.label} ${styles.type_normal}`}>Normal</em>
+                                                <span className={styles.txt}>해당 카드들은 손쉽게 구할 수 있습니다.</span>
+                                                <span className={styles.txt}>ex) 지원상점, 카드상자, 월드 보스 인베이젼</span>
+                                            </div>
+                                            <div className={styles.sub_info}>
+                                                <em className={`${styles.label} ${styles.type_rare}`}>Rare</em>
+                                                <span className={styles.txt}>해당 카드들은 손쉽게 구하기 어렵거나 과금을 통해 구할 수 있습니다.</span>
+                                                <span className={styles.txt}>ex) 희귀한 확률로 협동 플레이 보상, 과금형 이벤트 카드상자, 카드깡</span>
+                                            </div>
+                                            <div className={styles.sub_info}>
+                                                <em className={`${styles.label} ${styles.type_premium}`}>Premium</em>
+                                                <span className={styles.txt}>해당 카드들은 과금, 특정 컨텐츠에서만 얻을 수 있습니다.</span>
+                                                <span className={styles.txt}>ex) 월드보스상자, 카드깡, 레전더리 배틀</span>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -60,53 +125,61 @@ const cardDetail = (props) => {
                                     <div className={styles.info_group}>
                                         <h4 className={styles.title}>옵션</h4>
                                         <div className={styles.info}>
-                                            <div className={styles.value}>첫 번째 7.5% ~ 11%</div>
+                                            <div className={styles.value}><span className={styles.txt_index}>첫번째</span> 7.5% ~ 11%</div>
                                             <div className={styles.option}>
-                                                <em className={styles.label}>{info_detail.option1}</em>
+                                                {
+                                                    info_detail.option1.map((item,index) => (
+                                                        <em key={index} className={`${styles.label} ${classifyOptionToLabel(item)}`}>{item}</em>
+                                                    ))
+                                                }
                                             </div>
                                         </div>
                                         <div className={styles.info}>
-                                            <div className={styles.value}>두 번째 7.5% ~ 11%</div>
+                                            <div className={styles.value}><span className={styles.txt_index}>두번째</span> 7.5% ~ 11%</div>
                                             <div className={styles.option}>
-                                                <em className={styles.label}>{info_detail.option2}</em>
+                                                {
+                                                    info_detail.option2.map((item,index) => (
+                                                        <em key={index} className={`${styles.label} ${classifyOptionToLabel(item)}`}>{item}</em>
+                                                    ))
+                                                }
                                             </div>
                                         </div>
                                         <div className={styles.info}>
-                                            <div className={styles.value}>세 번째 5.1%</div>
+                                            <div className={styles.value}><span className={styles.txt_index}>세번째</span> 5.1%</div>
                                             <div className={styles.option}>
                                                 {
                                                     info_detail.option3.map((item,index) => (
-                                                        <em key={index} className={styles.label}>{item}</em>
+                                                        <em key={index} className={`${styles.label} ${classifyOptionToLabel(item)}`}>{item}</em>
                                                     ))
                                                 }
                                             </div>
                                         </div>
                                         <div className={styles.info}>
-                                            <div className={styles.value}>네 번째 5.4%</div>
+                                            <div className={styles.value}><span className={styles.txt_index}>네번째</span> 5.4%</div>
                                             <div className={styles.option}>
                                                 {
                                                     info_detail.option4.map((item,index) => (
-                                                        <em key={index} className={styles.label}>{item}</em>
+                                                        <em key={index} className={`${styles.label} ${classifyOptionToLabel(item)}`}>{item}</em>
                                                     ))
                                                 }
                                             </div>
                                         </div>
                                         <div className={styles.info}>
-                                            <div className={styles.value}>다섯번째 5.7%</div>
+                                            <div className={styles.value}><span className={styles.txt_index}>다섯번째</span> 5.7%</div>
                                             <div className={styles.option}>
                                                 {
                                                     info_detail.option5.map((item,index) => (
-                                                        <em key={index} className={styles.label}>{item}</em>
+                                                        <em key={index} className={`${styles.label} ${classifyOptionToLabel(item)}`}>{item}</em>
                                                     ))
                                                 }
                                             </div>
                                         </div>
                                         <div className={styles.info}>
-                                            <div className={styles.value}>여섯번째 6%</div>
+                                            <div className={styles.value}><span className={styles.txt_index}>여섯번째</span> 6%</div>
                                             <div className={styles.option}>
                                                 {
                                                     info_detail.option6.map((item,index) => (
-                                                        <em key={index} className={styles.label}>{item}</em>
+                                                        <em key={index} className={`${styles.label} ${classifyOptionToLabel(item)}`}>{item}</em>
                                                     ))
                                                 }
                                             </div>
