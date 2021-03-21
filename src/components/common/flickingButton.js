e9425d5d978685c669d5e045f5b88652b9ce8411
a23822 <a238220@gmail.com>
2021-03-21 11:20:59 +0900

    플리킹 이슈 수정

--- src/components/common/flickingButton.js
+++ src/components/common/flickingButton.js
@@ -21,24 +21,37 @@ const FlickingButton = (props) => {
     }, [flickingInfo])
 
 
-    const clickBtn = (dir) => {
+    const temp = (idx, delay) => {
+        console.log(idx);
+        flickingInfo.moveTo(idx, delay);
+        ui_actions.setTempCardFlickingIndex(idx);
+    }
+
+    const clickBtn = async(dir) => {
         // 카드정보에서 눌렀을 때
+        console.log(flickingInfo.getAllPanels().length);
+        let maxCardLength = flickingInfo.getAllPanels().length;
+        let movIdx = 0;
         if (flag === 'cardInfo') {
             if (dir === `left`) {
-                var movIdx = flickingInfo.getCurrentPanel().state.index-3;
-                flickingInfo.moveTo(movIdx, 300);
+                movIdx = (flickingInfo.getCurrentPanel().state.index-3);
+                if (movIdx < 0) {
+                    movIdx = maxCardLength+movIdx;
+                }
             } else {
-                var movIdx = flickingInfo.getCurrentPanel().state.index+3;
-                flickingInfo.moveTo(movIdx, 300);
+                movIdx = (flickingInfo.getCurrentPanel().state.index+3);
+                if (movIdx >= maxCardLength) {
+                    movIdx = movIdx%maxCardLength;
+                }
             }
-            ui_actions.setTempCardFlickingIndex(movIdx);
         }
+        await temp(movIdx, 300);
     }
 
     return (
         <div className={styles.flick_btn_area} style={{height: `${height}px`, marginTop: `-${height}px`}}>
             <button onClick={() => clickBtn('left')} type="button" className={`${styles.btn_move} ${styles.type_left}`}>이전</button>
-            <button onClick={() => clickBtn('right')} type="button" className={`${styles.btn_move} ${styles.type_right}`}>이후</button>
+            <button onClick={() => clickBtn('right')} type="button" className={`${styles.btn_move} ${styles.type_right}`}>다음</button>
         </div>
     )
 }
